1ef94f2f3431d0f7a9f87d411595eb0badb30223
Farhan Rahman Arnob <farhanarnob@live.com>
2017-06-07 11:23:56 +0600

    Working for home page viewpager.

---
+++ app/src/main/java/com/androlit/bookcloud/view/adapters/HomePagerAdapter.java
@@ -0,0 +1,25 @@
+package com.androlit.bookcloud.view.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by ${farhanarnob} on ${06-Oct-16}.
+ */
+
+public class HomePagerAdapter extends FragmentPagerAdapter {
+    public HomePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
